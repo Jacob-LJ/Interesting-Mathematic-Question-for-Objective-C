07dd0104eff4002391b9fb1fc28a140ffafcb902
Jacob <312846421@qq.com>
2017-03-13 22:51:11 +0800

    init

---
+++ BuyBeersQuestion/BuyBeersQuestion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuyBeersQuestion
+//
+//  Created by Jacob_Liang on 2017/3/13.
+//  Copyright © 2017年 Jacob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BuyBeersQuestion/BuyBeersQuestion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuyBeersQuestion
+//
+//  Created by Jacob_Liang on 2017/3/13.
+//  Copyright © 2017年 Jacob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
